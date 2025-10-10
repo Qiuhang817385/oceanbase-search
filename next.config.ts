b9a54461e3fd8f721348f0b2b23be174afc6042d
Qiuhang817385 <826266129@QQ.COM>
2025-10-10 13:52:10 +0800

    Enhance Next.js configuration with export output, TypeScript, and ESLint settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/static' : '',
+  // env: {
+  //   ASSET_PREFIX: process.env.NODE_ENV === 'production' ? '/static' : '',
+  // },
+}
 
-export default nextConfig;
+export default nextConfig
